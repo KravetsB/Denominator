b4c99f9b996e09ad2ed848f8b25adfbebdf444c3
ProgrammerBK <40270763+ProgrammerBK@users.noreply.github.com>
2019-02-21 17:00:10 +0200

    Add files via upload

---
+++ Denominator/Denominator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denominator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int devisor;
+            devisor = Convert.ToInt32(Console.ReadLine());
+
+            string str="";
+            for (int i = 1; i<= devisor; i++)
+            {
+                if ((devisor % i) == 0)
+                    str  += i.ToString() + "  ";
+                    
+            }
+            Console.WriteLine(str);
+            Console.ReadKey();
+
+        }
+    }
+}
